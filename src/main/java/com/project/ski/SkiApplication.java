d5b4644658f2e6c725e1033b638c6a8cc486923c
Younghwan <tacdo12@naver.com>
2022-10-09 14:50:52 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/ski/SkiApplication.java
@@ -0,0 +1,13 @@
+package com.project.ski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiApplication.class, args);
+	}
+
+}
