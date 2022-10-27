aff0bdd5760459a603b298c39f11e27537d959b5
김병조 <belongs2jo@gmail.com>
2022-10-27 17:14:32 +0900

    동호회 게시판 완료

---
+++ src/main/java/com/project/ski/repository/ClubRepository.java
@@ -0,0 +1,11 @@
+package com.project.ski.repository;
+
+import com.project.ski.domain.club.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClubRepository extends JpaRepository<Club,Long> {
+
+    List<Club> findAllByOrderByIdDesc();
+}
--- src/main/java/com/project/ski/web/dto/CmRespDto.java
+++ src/main/java/com/project/ski/web/dto/CmRespDto.java
@@ -1,8 +1,10 @@
 package com.project.ski.web.dto;
 
+import com.project.ski.domain.club.Club;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,5 @@ public class CmRespDto<T> {
     private int code;
     private String message;
     private T data;
+
 }
