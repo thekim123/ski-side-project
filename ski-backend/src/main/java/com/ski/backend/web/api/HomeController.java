ffa8c28f7d9dd5183dffbe2b34133d0c4ce92d6e
Younghwan <tacdo12@naver.com>
2022-12-26 12:26:37 +0900

    홈 추가

---
+++ ski-backend/src/main/java/com/ski/backend/web/api/HomeController.java
@@ -0,0 +1,14 @@
+package com.ski.backend.web.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
