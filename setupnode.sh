30b093c12cee5a7e07b1bd684b21f19096004bd1
Younghwan <tacdo12@naver.com>
2022-12-28 01:57:53 +0900

    sisisi

--- setupnode.sh
+++ setupnode.sh
@@ -1,5 +1,7 @@
 # node install
+curl -o- -L https://yarnpkg.com/install.sh | bash
+source ~/.bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 8.0.0
-npm install yarn -g
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
