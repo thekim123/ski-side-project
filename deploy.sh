1857b1ef5860d9e6b49148c771942e6ef9c86ea4
Younghwan <tacdo12@naver.com>
2022-12-29 03:34:00 +0900

    deploy.sh, run.sh 추가

--- deploy.sh
+++ deploy.sh
@@ -14,26 +14,4 @@ nvm install v16.13.0
 nvm use 16.13.0
 npm install -g npm@latest
 npm install -g yarn
-npm install -g serve
-
-# 4. git clone
-rm -rf ski-side-project
-git clone https://github.com/thekim123/ski-side-project.git
-
-# 4. run web-module
-cd ~/ski-side-project/ski-backend
-sudo chmod u+x gradlew
-./gradlew clean build
-cd ~/sski-side-project/ski-backend/build/libs
-nohup java -jar ski-0.0.1-SNAPSHOT.jar 1>backend-std.out 2>backend-err.out &
-
-# 5. run web-module
-cd ~/ski-side-project/sse-server
-sudo chmod u+x gradlew
-./gradlew clean build
-cd ~/ski-side-project/sse-server/build/libs
-nohup java -jar sse-server-0.0.1-SNAPSHOT.jar 1>backend-std.out 2>backend-err.out &
-
-# 6. run front-module
-cd ~/ski-side-project/frontend
-nohup npx serve -s build &
+npm install -g serve@11.3.2
