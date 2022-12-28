efd4ab3161eed0304c07b60d713775bcddebbf11
Younghwan <tacdo12@naver.com>
2022-12-29 03:32:44 +0900

    배포 스크립트, 실행스크립트 추가

---
+++ custom-deploy/run.sh
@@ -0,0 +1,18 @@
+# 1. run web-module
+echo "1. run web-module"
+cd ~/application
+chmod u+x ski-0.0.1-SNAPSHOT.jar
+nohup java -jar ski-0.0.1-SNAPSHOT.jar 1>backend-std.out 2>backend-err.out &
+
+# 2. run web-module
+echo "2. run web-module"
+chmod u+x sse-server-0.0.1-SNAPSHOT.jar
+nohup java -jar sse-server-0.0.1-SNAPSHOT.jar 1>sse-std.out 2>sse-err.out &
+
+# 3. run front-module
+echo "3. run front-module"
+nvm use 16.13.0
+chmod u+x build.zip
+mkdir build
+unzip build.zip -d build
+nohup npx serve -l 3000 -s build &
