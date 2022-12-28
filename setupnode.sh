7307373e574b791335b3d87207d9082748f8e12d
Younghwan <tacdo12@naver.com>
2022-12-28 19:28:27 +0900

    sdf

--- setupnode.sh
+++
@@ -1,7 +0,0 @@
-# node install
-curl -o- -L https://yarnpkg.com/install.sh | bash
-source ~/.bashrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
